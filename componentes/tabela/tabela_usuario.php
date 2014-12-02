d7277a81cd34c52184f206113367ce5abc95ebbf
Jc Neto <mapnetos@hotmail.com>
2014-12-02 09:32:39 -0300

    classe usuario

---
+++ componentes/tabela/tabela_usuario.php
@@ -0,0 +1,15 @@
+<?php
+	echo "<tr>
+		<td>$cod_usua[$i]</td>
+		<td>$desc_usua[$i]</td>
+		<td>$cod_usua[$i]</td>
+		<td>$cod_grpusuat[$i]</td>
+		<td>$emai_usua[$i]</td>
+		<td>$sta_usua[$i]</td>
+		<td>
+			<i class='fa fa-pencil' style='font-size:9pt; color:#6AA872; cursor:pointer;' data-toggle='modal' data-target='#myModal'></i> &nbsp;
+			<i class='glyphicon glyphicon-trash' style='font-size:9pt; color:#D85869; cursor:pointer;' data-toggle='modal' data-target='#myModal'></i>
+		</td>
+	</tr>";
+?>
+
