da0286aec92fd0e481ec85dd9c350a08547c98a6
Jc Neto <mapnetos@hotmail.com>
2014-12-02 17:58:27 -0300

    post.php

--- funcoes/php/post.php
+++ funcoes/php/post.php
@@ -5,14 +5,18 @@ require_once("post_funcoes.php");
 	  echo '<br>titulo'.$_POST['descrPost'];
       echo '<br>titulo'.$_POST['tipPost'];
       echo '<br>titulo'.$_POST['codDoc'];
-      echo '<br>titulo'.$_POST['codBibl']='null';
-      echo '<br>titulo'.$_POST['codUsua']='null';
-      echo '<br>titulo'.$_POST['dathorPost']='null';
+      echo '<br>titulo'.$_POST['codBibl']='';
+      echo '<br>titulo'.$_POST['codUsua']=1;
+      echo '<br>titulo'.$_POST['dathorPost']='';
       echo '<br>titulo'.$_POST['codCate'];
       echo '<br>titulo'.$_POST['staPost'];
       echo '<br>titulo'.$_POST['reqconfiPost'];
 	  echo '<br>titulo'.$_POST['hierarqPost'];
       echo '<br>titulo'.$_POST['unicPost'];
-      echo '<br>titulo'.$_POST['datexpirPost']='null';
+      echo '<br>titulo'.$_POST['datexpirPost']='';
+	  
+	gravaPost($titulo_post,$descr_post,$tip_post,$cod_doc,$cod_bibl,$cod_usua,$dathor_post,$cod_cate,$sta_post,$reqconfi_post,$hierarq_post,$unic_post,$datexpir_post);
+	
+	marciaa-
 	
 ?>
