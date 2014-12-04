5a542226129334f3e586084654e824dc8455ca30
Jc Neto <mapnetos@hotmail.com>
2014-12-04 17:42:14 -0300

    melhorado post

--- funcoes/php/post.php
+++ funcoes/php/post.php
@@ -2,21 +2,21 @@
 require_once("post_funcoes.php");
 	
 	$tituloPost		 =isset($_POST['tituloPost'])?$_POST['tituloPost']:'ttt'; echo $tituloPost;
-    /*$descrPost		 =$_POST['descrPost'];  -------
-    $tipPost		 =$_POST['tipPost'];
-    $codDoc			 =$_POST['codDoc'];
-    $codBibl		 =$_POST['codBibl']='';
-    $codUsua 		 =$_POST['codUsua']=1;
-    $urlVideo		 =$_POST['urlVideo']=1;
-    $dathorPost		 =$_POST['dathorPost']='';//
-    $codCate		 =$_POST['codCate'];
-    $staPost	     =$_POST['staPost'];
-    $reqconfiPost    =$_POST['reqconfiPost'];
-    $hierarqPost	 =$_POST['hierarqPost'];
-    $unicPost	 	 =$_POST['unicPost'];
-    $datexpirPost	 =$_POST['datexpirPost']='';
-    $imgPost	 	 =$_POST['imgPost'];
-	 //----------------------------------
+   $descrPost     =isset($_POST['descrPost'])?$_POST['descrPost']:"";  -------
+    $tipPost		 =isset($_POST['tipPost'])?$_POST['tipPost']:"";
+    $codDoc			 =isset($_POST['codDoc'])?$_POST['codDoc']:"";
+    $codBibl		 =isset($_POST['codBibl'])?$_POST['codBibl']:"";
+    $codUsua 		 =isset($_POST['codUsua'])?$_POST['codUsua']:"";
+    $urlVideo		 =isset($_POST['urlVideo'])?$_POST['urlVideo']:"";
+    $dathorPost		 =isset($_POST['dathorPost'])?$_POST['dathorPost']:"";//
+    $codCate		 =isset($_POST['codCate'])?$_POST['codCate']:"";
+    $staPost	     =isset($_POST['staPost'])?$_POST['staPost']:"";
+    $reqconfiPost    =isset($_POST['reqconfiPost'])?$_POST['reqconfiPost']:"";
+    $hierarqPost	 =isset($_POST['hierarqPost'])?$_POST['hierarqPost']:"";
+    $unicPost	 	 =isset($_POST['unicPost'])?$_POST['unicPost']:"";
+    $datexpirPost	 =isset($_POST['datexpirPost'])?$_POST['datexpirPost']:"";
+    $imgPost	 	 =isset($_POST['imgPost'])?$_POST['imgPost']:"";
+	 /* //----------------------------------
       echo '<br>titulo'.$_POST['tituloPost'];
 	  echo '<br>titulo'.$_POST['descrPost'];
       echo '<br>titulo'.$_POST['tipPost'];
@@ -32,44 +32,44 @@ require_once("post_funcoes.php");
       echo '<br>titulo'.$_POST['unicPost'];
       echo '<br>titulo'.$_POST['datexpirPost']='';
 	  echo '<br>titulo'.$_POST['imgPost'];
-	  
+	  */
 	gravaPost($_POST['tituloPost'],$_POST['descrPost'],$_POST['tipPost'],$cod_doc,$cod_bibl,$cod_usua,$dathor_post,$cod_cate,$sta_post,$reqconfi_post,$hierarq_post,$unic_post,$datexpir_post);
 	
-	if(isset($_POST['tipPost']) && $_POST['tipPost']==1){ // 1- Sem imagem . 2-imagem  3-video 4-galeria
-		$_POST['imgPost']='';
-		$_POST['codDoc']='';
-		$_POST['codBibl']='';
-		$_POST['urlVideo']='';
-	}else if((isset($_POST['tipPost']) && $_POST['tipPost']==2) && !empty($_POST['codDoc'])){
-		$_POST['imgPost']='';
-		//$_POST['codDoc']=''; este foi selecionado
-		$_POST['codBibl']='';
-		$_POST['urlVideo']='';
+	if($tipPost==1){ // 1- Sem imagem . 2-imagem  3-video 4-galeria
+		$imgPost='';
+		$codDoc='';
+		$codBibl='';
+		$urlVideo='';
+	}else if($tipPost==2 && !empty($codDoc)){
+		$imgPost='';
+		//$codDoc=''; este foi selecionado
+		$codBibl='';
+		$urlVideo='';
 		
-	}else if((isset($_POST['tipPost']) && $_POST['tipPost']==2) && (isset($_POST['imgPost']) && !empty($_POST['imgPost']))){
-		//$_POST['imgPost']='';este foi selecionado
-		$_POST['codDoc']='';
-		$_POST['codBibl']='';
-		$_POST['urlVideo']='';
-	}else if((isset($_POST['tipPost']) && $_POST['tipPost']==3) && (isset($_POST['codDoc']) && !empty($_POST['codDoc']))){
-		$_POST['imgPost']='';
-		//$_POST['codDoc']=''; este foi selecionado
-		$_POST['codBibl']='';
-		$_POST['urlVideo']='';
+	}else if($tipPost && $tipPost==2) && (isset($imgPost) && !empty($imgPost))){
+		//$imgPost='';este foi selecionado
+		$codDoc='';
+		$codBibl='';
+		$urlVideo='';
+	}else if($tipPost && $tipPost==3) && (isset($codDoc) && !empty($codDoc))){
+		$imgPost='';
+		//$codDoc=''; este foi selecionado
+		$codBibl='';
+		$urlVideo='';
 		
-	}else if((isset($_POST['tipPost']) &&$_POST['tipPost']==3) && (isset($_POST['urlVideo']) && !empty($_POST['urlVideo']))){
-		$_POST['imgPost']='';
-		$_POST['codDoc']='';
-		$_POST['codBibl']='';
-		//$_POST['urlVideo']='';este foi selecionado
-	}else if((isset($_POST['tipPost']) &&$_POST['tipPost']==4) && (isset($_POST['codDoc']) && !empty($_POST['codDoc']))){
-		$_POST['imgPost']='';
-		//$_POST['codDoc']=''; este foi selecionado
-		$_POST['codBibl']='';
-		$_POST['urlVideo']='';
+	}else if($tipPost &&$tipPost==3) && (isset($urlVideo) && !empty($urlVideo))){
+		$imgPost='';
+		$codDoc='';
+		$codBibl='';
+		//$urlVideo='';este foi selecionado
+	}else if($tipPost &&$tipPost==4) && (isset($codDoc) && !empty($codDoc))){
+		$imgPost='';
+		//$codDoc=''; este foi selecionado
+		$codBibl='';
+		$urlVideo='';
 		
 	}else{
 		echo "Erro na ilustração do post";
-	}*/
+	}//teste
 	
 ?>
