1a455622190bf3e770d528dd7cc6fead96699733
Jc Neto <mapnetos@hotmail.com>
2014-12-05 17:57:56 -0300

    iniac Doc

--- classes/Documento.php
+++ classes/Documento.php
@@ -12,18 +12,33 @@
 	var $icon_doc;
 
 	function setCod_doc($cod_doc){
-	$cod_doc
+		if($cod_doc > 0){
+			$this->cod_doc =$cod_doc;
+		}
 	function setDesc_doc($desc_doc){
-	$desc_doc
+		if(!empty($desc_doc)){
+			$this->desc_doc=$desc_doc; 
+		}
 	function setDatcria_doc($datcria_doc){
-	$datcria_doc
+		if(!empty($datcria_doc)){
+			$this->datcria_doc=$datcria_doc;
+	}
 	function setDatup_doc($datup_doc){
-	$datup_doc
+	// será criada por uma classe
+	}
+	
 	function setTam_doc($tam_doc){
-	$tam_doc
+		if(empty($tam_doc) && $tam_doc>0){
+			$this->tam_doc=$tam_doc;
+		}
+	}
 	function setLocal_doc($local_doc){
-	$local_doc
-	function setEstenc_doc(){
-	function setCod_usua(){
-	function setQtddow_doc(){
-	function setIcon_doc(){
+		if($local_doc
+	function setEstenc_doc($estenc_doc){
+	$estenc_doc
+	function setCod_usua($cod_usua){
+	$cod_usua
+	function setQtddow_doc($qtddow_doc){
+	$qtddow_doc
+	function setIcon_doc($qtddow_doc){
+	$qtddow_doc
